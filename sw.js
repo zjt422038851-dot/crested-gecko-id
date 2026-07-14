6838ed44a71aa547b7ad7ede793cc7af0264d230
zjt422038851-dot <zjt422038851-dot@users.noreply.github.com>
2026-07-14 19:01:33 +0800

    🎉 睫角守宫身份系统 v1.0

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'crested-gecko-v1';
+const URLS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js',
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => new Response('离线模式', {status: 503})))
+  );
+});
